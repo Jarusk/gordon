a5dae6d03c8aef3bdc3e7c3219cc7a49855f370b
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2015-09-09 09:18:31 -0400

    Changes project structure, created Makefile
    Signed-off-by: Matt Yaraskavitch <yaraskavitch.matt@gmail.com>

---
+++ src/helper.hpp
@@ -0,0 +1,6 @@
+#ifndef HELPER_HPP
+#define HELPER_HPP
+
+int add(int x, int y);
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "helper.hpp"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  cout << "Hello World!!!" << endl;
+  cout << add(5,6) << endl;
+  return 0;
+}
