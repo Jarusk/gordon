24b43b95074cd457035db6cfe6d72971c933c5ea
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2017-05-27 14:34:24 -0400

    Updated stddef types

--- include/stddef.h
+++ include/stddef.h
@@ -4,8 +4,8 @@
 #undef NULL
 #define NULL ((void *)0)
 
-//typedef unsigned int size_t;
-//typedef int ptrdiff_t;
+//typedef unsigned long size_t;
+//typedef void * ptrdiff_t;
 //typedef unsigned long wchar_t;
 
 #endif
