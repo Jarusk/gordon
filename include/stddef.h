705395db00285b3848f8dcb2cabf326b598f1531
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2017-05-28 19:19:29 -0400

    Implemented K&R C malloc, only temporary

--- include/stddef.h
+++ include/stddef.h
@@ -4,8 +4,8 @@
 #undef NULL
 #define NULL ((void *)0)
 
-//typedef unsigned long size_t;
-//typedef void * ptrdiff_t;
-//typedef unsigned long wchar_t;
+typedef unsigned long size_t;
+typedef void * ptrdiff_t;
+typedef unsigned long wchar_t;
 
 #endif
--- include/stdlib.h
+++ include/stdlib.h
@@ -1,6 +1,17 @@
 #ifndef STDLIB_H
 #define STDLIB_H
 
-#include <stdlib.h>
+//#include <stdlib.h>
+#include "stddef.h"
+
+#define EXIT_FAILURE -1
+#define EXIT_SUCCESS 0
+
+#define RAND_MAX 4294967295
+
+void *malloc(size_t);
+void free(void *);
+int rand(void);
+
 
 #endif
