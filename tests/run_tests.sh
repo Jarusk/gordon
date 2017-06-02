3218f3b24c61bc2c43376f8c7ea39c8d77f4197c
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2017-06-01 20:21:39 -0400

    test script now runs each test seperately after running the builds

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+SEP="----------------------------------------"
+
+
 for file in test_*.c
 do
 	gcc -g -L ../release/ -Wall -o $file.run $file -lgord
-	LD_LIBRARY_PATH=../release:$LB_LIBRARY_PATH ./$file.run
-	#rm $file.run
+done
+
+echo -e "\n\n"
+
+for file in test_*.c.run
+do
+	echo $SEP
+	echo -e "Running test: $file \n"
+	LD_LIBRARY_PATH=../release:$LB_LIBRARY_PATH ./$file
+	rm $file
+	echo -e "$SEP\n"
 done
