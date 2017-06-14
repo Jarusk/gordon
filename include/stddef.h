09e3d60af35f1c7bcef0d9cf13ca420fb9d44a55
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2017-06-14 14:19:45 -0400

    Use compiler brains

--- include/stddef.h
+++ include/stddef.h
@@ -4,10 +4,10 @@
 #undef NULL
 #define NULL ((void *)0)
 
-typedef unsigned long size_t;
+typedef __SIZE_TYPE__ size_t;
 typedef size_t off_t;
 typedef signed long ssize_t;
-typedef void * ptrdiff_t;
+typedef __PTRDIFF_TYPE__  ptrdiff_t;
 typedef unsigned long wchar_t;
 
 #endif
